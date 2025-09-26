33155a4e3647022c278837327774ffb98cdb54a3
DanielBritoSJunior <danielbritodasilvajunior@gmail.com>
2025-09-26 01:49:44 -0300

    Criação do arquivo global do tailwind-css para criação do carrossel de tecnologias

---
+++ portfolio-me/tailwind.config.js
@@ -0,0 +1,26 @@
+// tailwind.config.js
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Garante que o Tailwind escaneie todos os seus arquivos React
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      // Configuração da Animação de Carrossel
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-50%)' }, // Rola metade do conteúdo
+        }
+      },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite', // Nome, duração, estilo e repetição
+      },
+      // Fim da Configuração da Animação
+    },
+  },
+  plugins: [],
+};
